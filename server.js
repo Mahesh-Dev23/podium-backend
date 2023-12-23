b3c0cc0aecfd41d723810948c228c75bf52c1fd5
Mahesh-Dev23 <nomahesh@gmail.com>
2023-12-23 18:25:08 +0530

    server cors origin path added

--- server.js
+++ server.js
@@ -9,7 +9,9 @@
  const PORT = process.env.PORT || 5000
  const connectDB = require('./config/db')
  const app = express()
- app.use(cors())
+ app.use(cors({
+    origin: 'https://podium-demo-three.vercel.app/'
+ }))
  app.use(express.json())
  app.use(express.urlencoded({extended:false}))
 
