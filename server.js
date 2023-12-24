70fa58fff8a440f6bd852cb81265e6ed782af64a
Mahesh-Dev23 <nomahesh@gmail.com>
2023-12-24 12:58:40 +0530

    res header set to all

--- server.js
+++ server.js
@@ -17,11 +17,13 @@
  app.use(express.urlencoded({extended:false}))
 
  app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "https://podium-demo-three.vercel.app");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
+    // res.setHeader("Access-Control-Allow-Origin", "https://podium-demo-three.vercel.app");
+    // res.header(
+    //   "Access-Control-Allow-Headers",
+    //   "Origin, X-Requested-With, Content-Type, Accept"
+    // );
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', '*');
     next();
   });
   
