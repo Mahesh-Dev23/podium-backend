31de8108ab3ad49b78422b58171a7310fab56053
Mahesh-Dev23 <nomahesh@gmail.com>
2023-12-24 19:18:35 +0530

    userRouter changed

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,8 +3,15 @@ const router = express.Router()
 const { registerUser, loginUser, getMe, editUser, getAllUsers, getpackage } = require('../controllers/userController')
 const { protect } = require('../middleware/authMiddleware')
 
+const headers = {
+    'Access-Control-Allow-Origin': 'https://podium-demo-three.vercel.app',
+    'Access-Control-Allow-Headers': '*',
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Methods': 'POST, GET, PUT, DELETE'
+}
+
 router.post('/', registerUser)
-router.post('/login', loginUser)
+router.post('/login', loginUser, headers)
 router.get('/me', protect, getMe)
 router.put('/:id',  editUser)
 router.get('/', protect, getAllUsers)
