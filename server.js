5b71c4132a9338c4ddbdf075e9dbfbe8415edc37
Mahesh-Dev23 <84167070+Mahesh-Dev23@users.noreply.github.com>
2023-12-24 13:27:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@
     //   "Access-Control-Allow-Headers",
     //   "Origin, X-Requested-With, Content-Type, Accept"
     // );
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'https://podium-backend.vercel.app');
     res.header('Access-Control-Allow-Headers', '*');
     next();
   });
