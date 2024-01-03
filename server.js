8ec96a6ba2c3b3e831c9cb57065b51d989932229
Mahesh-Dev23 <nomahesh@gmail.com>
2024-01-03 21:48:27 +0530

    production serving index file

--- server.js
+++ server.js
@@ -42,122 +42,122 @@ const express = require('express')
  
  // ws scoket io data exhange ---------------------------------
 
-//  const io = new Server(httpServer, {
-//     cors: {
-//       origin: '*',
-//       methods: ['GET', 'POST']
-//     }
-//   });
+ const io = new Server(httpServer, {
+    cors: {
+      origin: '*',
+      methods: ['GET', 'POST']
+    }
+  });
 
-//   let room = ''
-//   let roomData = {}
-//   let message = ''
-//   let loggedUsers = []
-//   let admin = ''
-//   let user = {}
-//   let onePerson = ''
+  let room = ''
+  let roomData = {}
+  let message = ''
+  let loggedUsers = []
+  let admin = ''
+  let user = {}
+  let onePerson = ''
 
-// io.on("connection", (socket) => {
-//     socket.on('Logged In', (data)=> {
+io.on("connection", (socket) => {
+    socket.on('Logged In', (data)=> {
         
         
-//         console.log('Logged In from https://podium-backend.vercel.app', data)
-//         user = data
+        console.log('Logged In from https://podium-backend.vercel.app', data)
+        user = data
         
 
-//         // login first speaker
-//         if(loggedUsers.length === 0) {
-//             loggedUsers.push(data.name)
-//             socket.join([ data.room, data.name ])
-//             console.log('Logged In ', data)
-//             io.in( data.room ).emit('Welcome', data)
-//             io.in(user.room).emit('loggedUsers', loggedUsers)
-//         } 
+        // login first speaker
+        if(loggedUsers.length === 0) {
+            loggedUsers.push(data.name)
+            socket.join([ data.room, data.name ])
+            console.log('Logged In ', data)
+            io.in( data.room ).emit('Welcome', data)
+            io.in(user.room).emit('loggedUsers', loggedUsers)
+        } 
 
-//         // check if speaker is alrready logged in 
-//         if(loggedUsers.includes(data.name)) {
-//             console.log("user is already logged in")
-//         } else{
-//             loggedUsers.push(data.name)
-//             socket.join([ data.room, data.name ])
-//             console.log('Logged In ', data)
-//             io.in( data.room ).emit('Welcome', data)
-//             io.in(user.room).emit('loggedUsers', loggedUsers)
-//         } 
+        // check if speaker is alrready logged in 
+        if(loggedUsers.includes(data.name)) {
+            console.log("user is already logged in")
+        } else{
+            loggedUsers.push(data.name)
+            socket.join([ data.room, data.name ])
+            console.log('Logged In ', data)
+            io.in( data.room ).emit('Welcome', data)
+            io.in(user.room).emit('loggedUsers', loggedUsers)
+        } 
         
         
-//     })
+    })
 
     
-//     socket.on('Admin', (data) => {
-//         admin = data.name
-//     })
+    socket.on('Admin', (data) => {
+        admin = data.name
+    })
 
-//     socket.on('Slide to', (data) => {
-//         console.log('Slide to ', data)
-//         io.in(user.room).emit('go to', data)
-//     })
+    socket.on('Slide to', (data) => {
+        console.log('Slide to ', data)
+        io.in(user.room).emit('go to', data)
+    })
 
-//     // set single receiver admin to send individaul message
-//     socket.on('one paticipant', (data) => {
-//         console.log('one paticipant ', data)
-//         onePerson = data.name
-//     })
+    // set single receiver admin to send individaul message
+    socket.on('one paticipant', (data) => {
+        console.log('one paticipant ', data)
+        onePerson = data.name
+    })
 
-//     // 
-//     socket.on('paticipant', (data) => {
-//         console.log("onePerson ", onePerson)
-//         console.log('paticipant ', data)
+    // 
+    socket.on('paticipant', (data) => {
+        console.log("onePerson ", onePerson)
+        console.log('paticipant ', data)
 
-//         if(onePerson === '' && data.name === admin ){
-//             // admin to send message to all
-//             console.log("From Admin to All! ")
-//             io.in(user.room).emit('paticipant reply', data)
-//             // io.in(admin).emit('paticipant reply', data)
+        if(onePerson === '' && data.name === admin ){
+            // admin to send message to all
+            console.log("From Admin to All! ")
+            io.in(user.room).emit('paticipant reply', data)
+            // io.in(admin).emit('paticipant reply', data)
 
 
-//         }else if( onePerson === '' && data.name !== admin ) {
-//             // participant to send message to Admin and self
-//             console.log("To Admin ", data.name)
-//             io.in(data.name).emit('one person', data)
-//             io.in(admin).emit('one person', data)
+        }else if( onePerson === '' && data.name !== admin ) {
+            // participant to send message to Admin and self
+            console.log("To Admin ", data.name)
+            io.in(data.name).emit('one person', data)
+            io.in(admin).emit('one person', data)
 
-//         } else {
-//             // admin to send individaul message
-//             console.log("From Admin ", onePerson)
-//             io.in(onePerson).emit('one person', data)
-//             io.in(admin).emit('one person', data)
-//             onePerson = ''
-//         }
+        } else {
+            // admin to send individaul message
+            console.log("From Admin ", onePerson)
+            io.in(onePerson).emit('one person', data)
+            io.in(admin).emit('one person', data)
+            onePerson = ''
+        }
         
-//     })
+    })
 
-//     console.log(onePerson)
-//     console.log(loggedUsers)
+    console.log(onePerson)
+    console.log(loggedUsers)
 
-//     socket.on('logout', data => {
+    socket.on('logout', data => {
         
-//         if(data.name === '' || data.msg === '') return console.log("logout 122")
+        if(data.name === '' || data.msg === '') return console.log("logout 122")
         
-//         let newLoggedUser = []
-//         loggedUsers.filter( res => {  
-//             if( loggedUsers.includes(data.name) ) {
-//                 console.log("logout 128", data)
-//                 io.in(user.room).emit( 'loggedOutUsers', data)
-//             }else{
-//                 newLoggedUser.push(res)
-//             }
-//         })
+        let newLoggedUser = []
+        loggedUsers.filter( res => {  
+            if( loggedUsers.includes(data.name) ) {
+                console.log("logout 128", data)
+                io.in(user.room).emit( 'loggedOutUsers', data)
+            }else{
+                newLoggedUser.push(res)
+            }
+        })
 
-//         loggedUsers = []
-//         loggedUsers = newLoggedUser
+        loggedUsers = []
+        loggedUsers = newLoggedUser
 
-//         io.in(user.room).emit('loggedUsers', loggedUsers)
+        io.in(user.room).emit('loggedUsers', loggedUsers)
         
         
-//     })
+    })
  
-// }); 
+}); 
 
  
  
@@ -168,10 +168,10 @@ const express = require('express')
  if(process.env.NODE_ENV === 'production' ){
     // app.use(express.static(path.join(__dirname, '../frontend/build')))
     res.status(200).send("Welcome to Podium - DIY Virtual Meets")
-    // app.get('*', (req, res) =>{ 
-    //     // res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html')
-    //     res.sendFile(__dirname, './',  'index.html')
-    // })
+    app.get('*', (req, res) =>{ 
+        // res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html')
+        res.sendFile(__dirname, './',  'index.html')
+    })
  }else{
     app.get('/', (req, res)=>{
         res.status(200).send("Welcome to Podium - DIY Virtual Meets")
