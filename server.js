984a6a8964c0e765a3a0785a418f31690bc96095
Mahesh-Dev23 <nomahesh@gmail.com>
2024-01-04 15:15:23 +0530

    cors npm added

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const express = require('express')
  const PORT = process.env.PORT || 5000
  const connectDB = require('./config/db')
  const app = express()
-//  app.use(cors())
+ app.use(cors())
  app.use(express.json())
  app.use(express.urlencoded({extended:false}))
 // https://podium-backend.vercel.app
