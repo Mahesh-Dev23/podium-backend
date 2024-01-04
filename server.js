c90c3a27113d19a4c430ea5caa00e696e654f07f
Mahesh-Dev23 <nomahesh@gmail.com>
2024-01-04 17:46:01 +0530

    cors preflight

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const express = require('express')
  app.use(cors())
  app.use(express.json())
  app.use(express.urlencoded({extended:false}))
+ app.options('*', cors())
 // https://podium-backend.vercel.app
 //  app.use((req, res, next) => {
 //     // res.setHeader("Access-Control-Allow-Origin", "https://podium-demo-three.vercel.app");
