a6fcf3ad379dc7c618e065f407a40bbd2f605e79
Mahesh-Dev23 <nomahesh@gmail.com>
2024-01-05 10:57:09 +0530

    vercel token dded

--- server.js
+++ server.js
@@ -10,16 +10,20 @@ const express = require('express')
  const connectDB = require('./config/db')
  const app = express()
 
- var whitelist = ['https://podium-demo-three.vercel.app']
- var corsOptions = {
+ const vercelToken = 'prj_Xi9d9cmhuiwAyPtbgde4eAiAjZF7'
+ const whitelist = ['https://podium-demo-three.vercel.app', 'http://localhost:3000/']
+ const corsOptions = {
     origin: function (origin, callback) {
           if (whitelist.indexOf(origin) !== -1) {
           callback(null, true)
           } else {
           callback(new Error('Not allowed by CORS'))
           }
-          }
-      }
+          },
+    headers: {
+            Authorization: 'Bearer ' + vercelToken,
+        }
+ }
 
 
  app.use(cors(corsOptions))
