10594b88f7ea09c2d237e58b59570fae26acf464
Mahesh-Dev23 <nomahesh@gmail.com>
2024-01-17 13:03:21 +0530

    whitelist, cors oprtions disabled

--- server.js
+++ server.js
@@ -11,23 +11,23 @@ const express = require('express')
  const app = express()
 
  
- const whitelist = ['https://podium-demo-three.vercel.app', 'http://localhost:3000/']
- const corsOptions = {
-    origin: function (origin, callback) {
-          if (whitelist.indexOf(origin) !== -1) {
-          callback(null, true)
-          } else {
-          callback(new Error('Not allowed by CORS'))
-          }
-          },
-    headers: {
-            Authorization: 'Bearer ' + process.env.vercelToken,
-        },
-    methods : "GET,HEAD,PUT,PATCH,POST,DELETE"
- }
+//  const whitelist = ['https://podium-demo-three.vercel.app', 'http://localhost:3000/']
+//  const corsOptions = {
+//     origin: function (origin, callback) {
+//           if (whitelist.indexOf(origin) !== -1) {
+//           callback(null, true)
+//           } else {
+//           callback(new Error('Not allowed by CORS'))
+//           }
+//           },
+//     headers: {
+//             Authorization: 'Bearer ' + process.env.vercelToken,
+//         },
+//     methods : "GET,HEAD,PUT,PATCH,POST,DELETE"
+//  }
 
 
- app.use(cors(corsOptions))
+ app.use(cors())
  app.use(express.json())
  app.use(express.urlencoded({extended:false}))
  
